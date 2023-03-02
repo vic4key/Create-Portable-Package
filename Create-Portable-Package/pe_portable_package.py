e743bc363ed798d41a9a645d671a3649f1a1ed47
Vic P <vic4key@gmail.com>
2023-03-03 00:23:16 +0700

    Initial

--- Create-Portable-Package/pe_portable_package.py
+++ Create-Portable-Package/pe_portable_package.py
@@ -11,12 +11,12 @@ class PEPortablePackage:
   m_pe_object: PEPackage = None
 
   def __init__(self, file_path: str) -> None:
-    pe_format = vu.determine_pe_format(file_path)
-    if pe_format == vu.PEFormat.WIN:
+    pe_format = vu.determine_file_format(file_path)
+    if pe_format == vu.FileFormat.PE_WIN:
       self.m_pe_object = MZPackage(file_path)
-    elif pe_format == vu.PEFormat.LINUX:
+    elif pe_format == vu.FileFormat.PE_LINUX:
       self.m_pe_object = ELFPackage(file_path)
-    elif pe_format == vu.PEFormat.MACHO:
+    elif pe_format == vu.FileFormat.PE_MACHO:
       self.m_pe_object = MOPackage(file_path)
     else:
       raise NotImplementedError("The PE file format is not supported")
