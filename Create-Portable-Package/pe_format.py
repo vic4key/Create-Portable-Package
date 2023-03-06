0e7dd9ef5cf3e78300e04b5ca81615147f6d0bf1
Vic P <vic4key@gmail.com>
2023-03-06 15:17:47 +0700

    Initial

--- Create-Portable-Package/pe_format.py
+++ Create-Portable-Package/pe_format.py
@@ -5,9 +5,10 @@ import PyVutils as vu
 class PEPackage:
   ''' PE Package
   '''
-  m_object = None
+  m_file = None
   m_file_path = None
-  m_process_id = None
+  m_process = None
+  m_library_dirs = set()
   m_loaded_libraries = dict()
   m_exclusion_patterns = set()
 
@@ -20,7 +21,7 @@ class PEPackage:
 
     process_id = self._get_process_id()
     if process_id is None: raise RuntimeError("The PE file is not running.")
-    self.m_process_id = process_id
+    self.m_process = psutil.Process(process_id)
 
   def _get_file_name(self) -> str:
     return vu.extract_file_name(self.m_file_path)
@@ -31,18 +32,49 @@ class PEPackage:
       if process.name() == file_name: return process.pid
     return None
 
-  def _run_cli_command(self, command) -> str:
-    result = sp.run(command.split(" "), stdout=sp.PIPE)
-    return result.stdout.decode("utf-8")
+  def _is_excluded_file(self, file_name: str) -> bool:
+    for exclusion_pattern in self.m_exclusion_patterns:
+      l = vu.regex(file_name, exclusion_pattern)
+      if len(l) > 0: return True
+    return False
+
+  def _find_real_shared_library(self, file_name: str) -> str:
+    for dir in self.m_library_dirs:
+      file_path = os.path.join(dir, file_name)
+      if os.path.isfile(file_path):
+        if os.path.islink(file_path):
+          file_path = os.path.join(dir, os.readlink(file_path))
+        return file_path
+    return None
 
   def _find_shared_library(self, file_name: str) -> dict:
-    raise NotImplementedError("_find_shared_library")
+    if file_name in self.m_loaded_libraries.keys():
+      return self.m_loaded_libraries[file_name]
+
+    # print(f"Walking: {file_name}")
+
+    real_file_path = self._find_real_shared_library(file_name)
+    real_file_name = vu.extract_file_name(real_file_path)
+
+    result = {
+      "file_name": real_file_name,
+      "file_dir": vu.extract_file_directory(real_file_path),
+      "symbolic_name": file_name if file_name != real_file_name else None,
+    }
+
+    self.m_loaded_libraries[file_name] = result
+
+    return result
 
   def _find_shared_libraries(self, object, recursive) -> map:
     raise NotImplementedError("_find_shared_libraries")
 
   def create_portable_package(self, directory: str = ".", exclusion_files: str = None, force: bool = False):
+
+    # Loading exclusion files ...
+
     if exclusion_files and type(exclusion_files) is str:
+      print("Loading the exclusion files ...")
       for exclusion_file in exclusion_files.split(';'):
         try:
           with open(exclusion_file, "r") as f:
@@ -51,8 +83,14 @@ class PEPackage:
             exclusion_patterns = list(filter(lambda line: len(line) > 0, exclusion_patterns))
             self.m_exclusion_patterns.update(exclusion_patterns)
         except: pass
-    
-    print("Looking for the dependency shared libraries ...")
+
+    print("Looking for searching directories ...")
+
+    for m in self.m_process.memory_maps():
+      if os.path.isfile(m.path):
+        self.m_library_dirs.add(vu.extract_file_directory(m.path))
+
+    print("Looking for dependency shared libraries ...")
 
     pe_file_name = self._get_file_name()
     package_dir = os.path.join(directory, pe_file_name)
@@ -60,7 +98,7 @@ class PEPackage:
     if force and os.path.isdir(package_dir): shutil.rmtree(package_dir)
     if not os.path.isdir(package_dir): os.makedirs(package_dir)
 
-    shared_libraries = self._find_shared_libraries(self.m_object, True)
+    shared_libraries = self._find_shared_libraries(self.m_file, True)
 
     print("Copying the dependency shared libraries ...")
 
@@ -71,22 +109,26 @@ class PEPackage:
       dst = os.path.join(package_dir, pe_file_name)
       shutil.copyfile(src, dst)
 
-      print(f"\t{index}. '{src}' => '{dst}'")
+      print(f"\t{index:3}. Copying '{src}' => '{dst}'")
       index += 1
 
     for _, e in shared_libraries.items():
+      msg = f"\t{index:3}. "
       file_name = e["file_name"]
-      src = os.path.join(e["file_dir"], file_name)
-      dst = os.path.join(package_dir, file_name)
-      shutil.copyfile(src, dst)
+      if self._is_excluded_file(file_name):
+        msg += f"Ignored '{file_name}'"
+      else:
+        msg += f"Copying '{file_name}' from '{src}'"
 
-      msg = f"\t{index}. '{file_name}' -> '{src}'"
-
-      if e["symbolic"]:
-        symbolic_name = e["symbolic_name"]
-        dst = os.path.join(package_dir, symbolic_name)
+        src = os.path.join(e["file_dir"], file_name)
+        dst = os.path.join(package_dir, file_name)
         shutil.copyfile(src, dst)
-        msg += f" ('{symbolic_name}')"
+
+        if e["symbolic_name"]:
+          symbolic_name = e["symbolic_name"]
+          dst = os.path.join(package_dir, symbolic_name)
+          shutil.copyfile(src, dst)
+          msg += f" ('{symbolic_name}')"
 
       print(msg)
       index += 1
--- Create-Portable-Package/pe_format_elf.py
+++ Create-Portable-Package/pe_format_elf.py
@@ -11,41 +11,9 @@ class ELFPackage(PEPackage):
   def __init__(self, file_path: str) -> None:
     PEPackage.__init__(self, file_path)
     try:
-      self.m_object = ELFFile(open(self.m_file_path, "rb"))
-      if self.m_object is None: raise RuntimeError("Could not load the ELF file.")
+      self.m_file = ELFFile(open(self.m_file_path, "rb"))
+      if self.m_file is None: raise RuntimeError("Could not load the ELF file.")
     except Exception as e: print(str(e))
-
-  def _find_shared_library(self, file_name: str) -> dict:
-    if file_name in self.m_loaded_libraries.keys():
-      return self.m_loaded_libraries[file_name]
-
-    # print(f"Walking: {file_name}")
-
-    lines = []
-    output = self._run_cli_command(f"lsof -p {self.m_process_id}")
-    for line in output.split("\n"):
-      if file_name in line: lines.append(line)
-    assert len(lines), "more than one loaded module"
-
-    line_parts = list(filter(lambda e: len(e) > 0, lines[0].split(' ')))
-    assert len(line_parts) >= 9 # 9 columns
-
-    shared_library_file_path = line_parts[8]
-    if os.path.islink(shared_library_file_path):
-      assert False, "resolve symbolic link here"
-
-    tmp = vu.extract_file_name(shared_library_file_path)
-
-    result = {
-      "symbolic": file_name != tmp,
-      "file_name": tmp,
-      "file_dir": vu.extract_file_directory(shared_library_file_path),
-      "symbolic_name": file_name,
-    }
-
-    self.m_loaded_libraries[file_name] = result
-
-    return result
 
   def _find_shared_libraries(self, object, recursive) -> map:
     shared_libraries = {}
@@ -53,14 +21,13 @@ class ELFPackage(PEPackage):
     for section in object.iter_sections():
       if not isinstance(section, DynamicSection):
         continue
-
       if section.num_tags() > 0:
         for tag in section.iter_tags():
-          if tag.entry.d_tag == 'DT_NEEDED':
+          if tag.entry.d_tag == "DT_NEEDED":
             shared_library = self._find_shared_library(tag.needed) 
             shared_libraries[tag.needed] = shared_library
 
-    if recursive and len(shared_libraries) > 0:
+    if recursive:
       recursive_shared_libraries = {}
       for e in shared_libraries.values():
         file_path = os.path.join(e["file_dir"], e["file_name"])
